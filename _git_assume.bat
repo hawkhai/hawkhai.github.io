d62f988ac572ad16eb6e220682405712a7b9b883
yangquanhai <haihoing@qq.com>
2020-09-02 16:41:31 +0800

    git update-index --assume-unchanged Gemfile.lock

--- _git_assume.bat
+++ _git_assume.bat
@@ -1,3 +1,4 @@
 # git 忽略本地已存在文件的修改
 # 将文件修改忽略
 git update-index --assume-unchanged _config.yml
+git update-index --assume-unchanged Gemfile.lock
--- _git_no-assume.bat
+++ _git_no-assume.bat
@@ -1,3 +1,4 @@
 # git 忽略本地已存在文件的修改
 # 取消文件忽略
 git update-index --no-assume-unchanged _config.yml
+git update-index --no-assume-unchanged Gemfile.lock
