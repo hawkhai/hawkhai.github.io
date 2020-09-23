22bd060284bda63004fb82dbed7fd959613980af
yangquanhai <haihoing@qq.com>
2020-09-23 21:58:58 +0800

    cdn.bootcss.com/mathjax/2.7.5/MathJax.js

--- assets/mathjax.js
+++ assets/mathjax.js
@@ -6,6 +6,7 @@ function MathJaxHubConfig() {
     }
 
     MathJax.Hub.Config({
+        showProcessingMessages: false,
         jax: ["input/TeX", "output/HTML-CSS"],
         tex2jax: {
             inlineMath: [['$', '$'], ["\\(", "\\)"]],
@@ -14,7 +15,7 @@ function MathJaxHubConfig() {
             skipTags: ['script', 'noscript', 'style', 'textarea', 'pre', 'code']
         },
         messageStyle: "none",
-        "HTML-CSS": {preferredFont: "TeX", availableFonts: ["STIX", "TeX"]},
+        "HTML-CSS": {preferredFont: "TeX", availableFonts: ["STIX", "TeX"], showMathMenu: false},
         TeX: {
             equationNumbers: {autoNumber: "AMS"},
             extensions: ["AMSmath.js", "AMSsymbols.js", "AMScd.js"],
