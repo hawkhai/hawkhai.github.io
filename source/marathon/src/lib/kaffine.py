a75efe141338736084128a4b924c42b4224400ad
haihoing <haihoing@qq.com>
2020-10-25 01:01:26 +0800

    rename var

--- source/marathon/src/lib/kaffine.py
+++ source/marathon/src/lib/kaffine.py
@@ -16,26 +16,26 @@ def affine(img, a, b, c, d, tx, ty):
     img[1:H+1, 1:W+1] = imgsrc
 
     # get new image shape
-    H_new = np.round(H * d).astype(np.int)
-    W_new = np.round(W * a).astype(np.int)
-    out = np.zeros((H_new+1, W_new+1, C), dtype=np.float32)
+    newH = np.round(H * d).astype(np.int)
+    newW = np.round(W * a).astype(np.int)
+    out = np.zeros((newH+1, newW+1, C), dtype=np.float32)
 
     # get position of new image
-    x_new = np.tile(np.arange(W_new), (H_new, 1))
-    y_new = np.arange(H_new).repeat(W_new).reshape(H_new, -1)
+    newx = np.tile(np.arange(newW), (newH, 1))
+    newy = np.arange(newH).repeat(newW).reshape(newH, -1)
 
     # get position of original image by affine
     adbc = a * d - b * c
-    x = np.round((d * x_new  - b * y_new) / adbc).astype(np.int) - tx + 1
-    y = np.round((-c * x_new + a * y_new) / adbc).astype(np.int) - ty + 1
+    x = np.round((d * newx  - b * newy) / adbc).astype(np.int) - tx + 1
+    y = np.round((-c * newx + a * newy) / adbc).astype(np.int) - ty + 1
 
     x = np.minimum(np.maximum(x, 0), W+1).astype(np.int)
     y = np.minimum(np.maximum(y, 0), H+1).astype(np.int)
 
     # assgin pixcel to new image
-    out[y_new, x_new] = img[y, x]
+    out[newy, newx] = img[y, x]
 
-    out = out[:H_new, :W_new]
+    out = out[:newH, :newW]
     out = out.astype(np.uint8)
 
     return out
