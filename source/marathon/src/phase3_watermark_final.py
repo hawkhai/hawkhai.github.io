420e20b91ba62a80a64ce63c6ab63b43901b9486
haihoing <haihoing@qq.com>
2020-10-25 02:42:36 +0800

    去水印 # 频域

--- source/marathon/src/phase3_watermark_final.py
+++ source/marathon/src/phase3_watermark_final.py
@@ -12,29 +12,29 @@ from lib.kinterpolate import blInterpolate
 # 频域
 
 if __name__ == "__main__":
-    srcInputFile=r"./output_images/phase2/phase2_broken_bl.jpg"
-    waterMarkFile=r"./input_images/phase3/phase3_watermark.jpg"
-    waterMarkFile2=r"./input_images/phase3/phase3_watermark_64_64.jpg"
-    dstOutputFile=r"./output_images/phase3/phase3_broken2_bl_with_wm.jpg"
-    recoverWaterMark=r"./output_images/phase3/phase3_watermark_recover.jpg"
+    srcInputFile = r"./output_images/phase2/phase2_broken_bl.jpg"
+    waterMarkFile = r"./input_images/phase3/phase3_watermark.jpg"
+    waterMarkFile2 = r"./output_images/phase3/phase3_watermark_64x64.jpg"
+    dstOutputFile = r"./output_images/phase3/phase3_broken_bl_with_wm.jpg"
+    recoverWaterMark = r"./output_images/phase3/phase3_watermark_recover.jpg"
 
-    #base 常量
-    fixed_wm_shape=(64,64)
-    random_seed_wm,random_seed_dct,mod,mod2=4399,2333,36,20
+    # base 常量
+    fixed_wm_shape = (64,64)
+    random_seed_wm,random_seed_dct,mod,mod2 = 4399,2333,36,20
 
-    wm=cv2.imread(waterMarkFile)
+    wm = cv2.imread(waterMarkFile)
     H, W, C = wm.shape
     ax = 64.0 / H
     ay = 64.0 / W
-    wm=blInterpolate(wm,ay,ax)
-    cv2.imwrite(waterMarkFile2,wm)
+    wm = blInterpolate(wm,ay,ax)
+    cv2.imwrite(waterMarkFile2, wm)
 
     bwm1 = watermark.Watermark(random_seed_wm,random_seed_dct,mod,mod2)
     bwm1.read_ori_img(srcInputFile)
     bwm1.read_wm(waterMarkFile2)
-    #写入水印
+    # 写入水印
     bwm1.embed(dstOutputFile)
 
-    #解出水印
+    # 解出水印
     bwm1 = watermark.Watermark(random_seed_wm,random_seed_dct,mod,mod2,wm_shape=fixed_wm_shape)
-    bwm1.extract(dstOutputFile,recoverWaterMark)
+    bwm1.extract(dstOutputFile, recoverWaterMark)
