17488f05c0f68e9947fe09c857b384ca206322b5
haihoing <haihoing@qq.com>
2022-05-23 00:37:53 +0800

    kaoyu/kaoyu_menu.py

--- kaoyu/kaoyu_menu.py
+++ kaoyu/kaoyu_menu.py
@@ -12,7 +12,13 @@ from pythonx.funclib import *
 from PIL import Image
 
 total = 30
-subnum = 3
+subnum = 5
+
+targetmap = [
+[( 1, 20), ( 2, 19), ( 3, 18)], [( 4, 17), ( 5, 16), (-1, -1)],
+[( 6, 25), ( 7, 24), ( 8, 23)], [( 9, 22), (10, 21), (-1, -1)],
+[(11, 30), (12, 29), (13, 28)], [(14, 27), (15, 26), (-1, -1)],
+]
 
 def mainsplit(rootdir, imgpath, num):
     width = 3174
@@ -21,14 +27,24 @@ def mainsplit(rootdir, imgpath, num):
     theight = height
     base = num * subnum
 
+    assert abs(twidth / theight - 140 / 297) <= 0.01, (twidth, theight)
+
     targetimg = Image.new(mode="RGB", size=(twidth * subnum, theight), color="#FFFFFF")
     for i in range(subnum):
         srcimg = Image.open(rootdir+ (imgpath % (base+i + 1)))
-        img = srcimg.crop((int(width/6), 0, int(width * 5 / 6), height))
-        targetimg.paste(img, (twidth*i, 0))
-    targetimg = targetimg.resize((int(6348/2), int(4490/2)))
-    targetimg.save(rootdir+ ("kz\\.%d.jpg" % num))
+        tmpimg = srcimg.crop((int(width/6), 0, int(width * 5 / 6), height))
+        targetimg.paste(tmpimg, (twidth*i, 0))
+
+    print(targetimg.size)
+    # 210mm×297mm
+    targetimg = targetimg.resize((int(2970 * 5 / 3), 2100))
+    # https://www.bilibili.com/read/cv10342533
+    targetimg = targetimg.convert("CMYK")
+    targetpath = rootdir+ ("kz\\rst.%d.jpg" % num)
+    writefile(targetpath, "")
+    osremove(targetpath)
+    targetimg.save(targetpath, "jpeg")
 
 if __name__ == "__main__":
-    for i in range(int(total/subnum)):
-        mainsplit(r"D:\kSource\blog\现杀现烤 _ 重庆烤鱼·扎啤\\", r"现杀现烤 _ 重庆烤鱼·扎啤_%d.png", i)
+    for idx in range(int(total/subnum)):
+        mainsplit(r"C:\kSource\blog\kaoyu\现杀现烤 _ 重庆烤鱼·扎啤\\", r"现杀现烤 _ 重庆烤鱼·扎啤_%d.png", idx)
