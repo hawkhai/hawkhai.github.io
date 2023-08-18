4ed4551b07d4706825a00bf7fe0995e055a296db
haihoing <haihoing@qq.com>
2023-08-18 22:08:30 +0800

    checkindex.py

---
+++ kvision/ksample/checkindex.py
@@ -0,0 +1,61 @@
+#encoding=utf8
+import re, os, sys
+for reldirx, _lidir in (("./", []), (os.path.split(__file__)[0]+"/", [])):
+    while not _lidir and len(reldirx) <= 100:
+        reldirx += "../"
+        checkfunc = lambda idir: os.path.exists(reldirx+idir+"/pythonx/funclib.py")
+        _lidir = [reldirx+idir for idir in os.listdir(reldirx) if checkfunc(idir)]
+        if _lidir: reldirx = _lidir[0]
+    if _lidir: break
+sys.path.append(reldirx)
+from pythonx.funclib import *
+from PIL import Image
+
+def mainDatamapJson(fdir, name):
+    pass
+
+def mainDatamapText(fdir, name):
+    pass
+
+typeset = set()
+def mainMd5(fdir, name):
+    fpath = os.path.join(fdir, name)
+    lines = readfileLines(fpath)
+    #01b90fd8d91802839de893282ec35dff "toyimg\hackathon\src\lib\fft.py" 9582
+    for line in lines:
+        li = re.findall("^([0-9a-f]{32}) \"(.*?)\" [0-9]+$", line)
+        assert li, line
+        imd5, ipath = li[0]
+        #print(imd5, ipath)
+        idir, iname = os.path.split(ipath)
+        inamec, itype = os.path.splitext(iname)
+        if itype.lower() in (".py", ".txt", ".json", ".png", ".jpg", ".jpeg", ".svg", ".ttf",):
+            continue
+        typeset.add(itype)
+        print(idir, inamec, itype)
+
+def maink():
+    lines = readfileLines("checkindex.txt")
+    for line in lines:
+        line = line.strip()
+        if not line: continue
+
+        fdir, fname = os.path.split(line)
+        print(fdir, fname)
+        if fname == "datamap.json":
+            mainDatamapJson(fdir, fname)
+
+        elif fname == "datamap.txt":
+            mainDatamapText(fdir, fname)
+
+        elif fname == "datamd5.txt":
+            mainMd5(fdir, fname)
+
+        else:
+            assert False, (fdir, fname)
+
+    print(typeset)
+
+if __name__ == "__main__":
+    maink()
+    print("ok")
---
+++ kvision/ksample/mytable/autogen_mytable_mdfile.py
@@ -0,0 +1,87 @@
+#encoding=utf8
+import re, os, sys
+for reldirx, _lidir in (("./", []), (os.path.split(__file__)[0]+"/", [])):
+    while not _lidir and len(reldirx) <= 100:
+        reldirx += "../"
+        checkfunc = lambda idir: os.path.exists(reldirx+idir+"/pythonx/funclib.py")
+        _lidir = [reldirx+idir for idir in os.listdir(reldirx) if checkfunc(idir)]
+        if _lidir: reldirx = _lidir[0]
+    if _lidir: break
+sys.path.append(reldirx)
+from pythonx.funclib import *
+import time, math
+
+REBUILD = "rebuild" in sys.argv
+
+def mainf2(rootdir, mdfile):
+    fdata = readfile(mdfile, True, "utf8")
+    fdata = fdata.split("-----")
+    assert len(fdata) == 2, len(fdata)
+    fdata = fdata[0].strip()
+    fdata += r"""
+
+-----
+"""
+
+    xtime = time.time()
+    svgtrue = {}
+
+    dirs = os.listdir(rootdir)
+    dirs.sort()
+    for dir in dirs:
+        if not dir.split(".")[-1] in ("jpg", "png", "jpeg"):
+            continue
+
+        imgurl = "https://cvsample.sunocean.life/mytable/{}".format(dir)
+
+        fname = dir
+        fpath = os.path.join(rootdir, fname)
+        fnamec = fname.split(".")[0]
+
+        item = r"""
+{}.xlsx 下载
+<a href="//cvsample.sunocean.life/mytable/result/{}.xlsx">自研</a> /
+<a href="//cvsample.sunocean.life/mytable/BaiduOCRConverter_Excel/{}.xlsx">百度</a> /
+<a href="//cvsample.sunocean.life/mytable/docmind/{}.xlsx">阿里</a>。
+
+{} include imagek5.html
+url="/mytable/{}" width="22%"
+url2="/mytable/result/{}.xlsx.png" width2="22%"
+url3="/mytable/BaiduOCRConverter_Excel/{}.xlsx.png" width3="22%"
+url4="/mytable/docmind/{}.xlsx.png" width4="22%"
+thumbnail="/thumbnail.png"
+{}
+""".format(fnamec, fnamec, fnamec, fnamec,
+           "{%",
+           fname, fnamec, fnamec, fnamec,
+           "%}",)
+
+        fdata += item
+
+        # mytable/docmind/007_c5918.xlsx.svg
+        # mytable/docmind/007_c5918.xlsx.svg/70401f2d8e4971c3a1fe179c661e2e27.007_c5918.xlsx.svg
+        docmind_svgfile = os.path.join(rootdir, "docmind", "{}.xlsx.svg".format(fnamec))
+        baidu_svgfile = os.path.join(rootdir, "BaiduOCRConverter_Excel", "{}.xlsx.svg".format(fnamec))
+        my_svgfile = os.path.join(rootdir, "result", "{}.xlsx.svg".format(fnamec))
+        for svgfile in (my_svgfile, baidu_svgfile, docmind_svgfile):
+            assert os.path.exists(svgfile)
+            fmd5 = getFileMd5(svgfile)
+            ipath = os.path.relpath(svgfile, rootdir).replace("\\", "/")
+            osspath = ipath + "/{}.{}.xlsx.svg".format(fmd5, fnamec)
+            print(fmd5, osspath)
+            svgtrue[ipath] = osspath
+
+    #print(writefile(mdfile, fdata, "utf8"))
+
+    fdata = fdata.replace(".xlsx.png", ".xlsx.svg")
+    #for key in svgtrue.keys():
+    #    fdata = fdata.replace(key, svgtrue[key])
+
+    print(writefile(mdfile, fdata, "utf8"))
+    #openTextFile(mdfile)
+
+if __name__ == "__main__":
+    from pythonx.note.kvision.pack import LcMapPath
+    mainf2(LcMapPath(r"E:\kSource\blog\kvision\ksample\mytable"),
+           LcMapPath(r"E:\kSource\blog\invisible\kvision\mytable.md"))
+    print("ok")
