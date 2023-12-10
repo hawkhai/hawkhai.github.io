dbd63165fdc3a49e2d5423b6c2e275e57ef896c1
haidell <haihoing@qq.com>
2023-12-11 00:41:53 +0800

    logdata_baidu.py

--- kvision/imgclassify/logdata_baidu.py
+++ kvision/imgclassify/logdata_baidu.py
@@ -37,6 +37,58 @@ def main():
             addkey(keyword, keywordmap)
             addkey(root, rootmap)
 
+    checkok = readfile("checkok.txt", True)
+    checktag = False
+    def mainfile2(fpath, fname, ftype):
+
+        nonlocal checktag
+        if not checkok or checkok == fpath:
+            checktag = True
+        if not checktag:
+            return
+
+        if not re.findall("[a-f0-9]{7}.json$", fname):
+            return
+        print(fpath)
+        fjson = readfileJson(fpath, "utf8")
+        result = fjson["result"]
+
+        keyword0 = result[0]["keyword"]
+        root0 = result[0]["root"].split("-")[0]
+
+        keyword1 = result[1]["keyword"]
+        root1 = result[1]["root"].split("-")[0]
+
+        rootall = [i["root"].split("-")[0] for i in result]
+        rootall = list(set(rootall))
+        rootall.sort()
+
+        if root0 and root0 == root1:
+            return
+
+        if root0 == "动物" and rootall in (
+                ["动物", "建筑"],
+                ["动物", "商品"],):
+            return
+        print(rootall)
+
+        # D:\kSource\blog\kvision\imgclassify\dataset\animal\cadb_018c8fe.jpg
+        # D:\kSource\blog\kvision\imgclassify\logdata\baidu\dataset\animal\cadb_018c8fe.json
+        imgfile = fpath.replace(r"logdata\baidu\dataset", r"dataset")
+        imgfilec, imgfilet = os.path.splitext(imgfile)
+        for ft in (".jpg", ".jpeg", ".webp", ".png"):
+            if os.path.exists(imgfilec+ft):
+                imgfile = imgfilec+ft
+            if os.path.exists(imgfile):
+                break
+        assert os.path.exists(imgfile), imgfile
+
+        openTextFile(fpath)
+        openFileWithDefault(imgfile)
+        assert False, fpath
+
+        writefile("checkok.txt", fpath)
+
     if False:
         searchdir(r"logdata\baidu\dataset", mainfile)
 
@@ -52,6 +104,8 @@ def main():
         cats = list(cats)
 
         print("\r\n".join(cats))
+
+    searchdir(r"logdata\baidu\dataset", mainfile2)
 
 if __name__ == "__main__":
     main()
