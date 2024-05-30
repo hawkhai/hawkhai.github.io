634625596a9ac9af7d0a67f90fbd8ca66d2f798a
yqh <haihoing@qq.com>
2024-05-30 17:54:35 +0800

    _checkfunc = lambda idir: os.path.exists(os.path.join(reldirx, idir, "pythonx", "funclib.py"))

--- kvision/ksample/sample_pack.py
+++ kvision/ksample/sample_pack.py
@@ -1,12 +1,12 @@
 #encoding=utf8
 import re, os, sys
 for reldirx, _lidir in (
-        (os.path.split(os.path.abspath(__file__))[0]+"/", []), # 这个先，规避加载漏洞。
-        (os.path.split(os.path.abspath("."))[0]+"/", [])):
+        (os.path.dirname(os.path.abspath(__file__)), []), # 这个先，规避加载漏洞。
+        (os.path.dirname(os.path.abspath(".")), [])):
     while not _lidir and len(reldirx) > 3: # 3 应该所有平台都问题不大。
-        reldirx = os.path.split(reldirx)[0] # 只尝试 funclib.py，funclib.pyc 存在版本。
-        _checkfunc = lambda idir: os.path.exists(reldirx+idir+"/pythonx/funclib.py")
-        _lidir = [reldirx+idir for idir in os.listdir(reldirx) if _checkfunc(idir)]
+        reldirx = os.path.dirname(reldirx) # 只尝试 funclib.py，funclib.pyc 存在版本。
+        _checkfunc = lambda idir: os.path.exists(os.path.join(reldirx, idir, "pythonx", "funclib.py"))
+        _lidir = [os.path.join(reldirx, idir) for idir in os.listdir(reldirx) if _checkfunc(idir)]
         assert len(_lidir) in (0, 1), _lidir
         if _lidir: reldirx = os.path.abspath(_lidir[0])
         del _checkfunc
