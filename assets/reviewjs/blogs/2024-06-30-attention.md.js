3b63771b00d03461cec8dd585bf2270a4928893e
notebook <haihoing@qq.com>
2024-07-21 01:56:29 +0800

    title: "机器学习 -- Attention is all you need"

--- assets/reviewjs/blogs/2024-06-30-attention.md.js
+++ assets/reviewjs/blogs/2024-06-30-attention.md.js
@@ -1,1 +1,1 @@
-document.write("2024-07-08: review"); // md5src=8195d415543ec6c8bcf1cb8d650c7705
+document.write("2024-07-08: review"); // md5src=dec421d4573241e627277e685f12dd11
--- assets/reviewjs/blogs/2024-07-09-ml-DL-limu.md.js
+++ assets/reviewjs/blogs/2024-07-09-ml-DL-limu.md.js
@@ -1,1 +1,1 @@
-document.write("2024-07-14: review"); // md5src=a40734d6588c095cf10380c72dde2b91
+document.write("2024-07-20: review"); // md5src=0f41373d23ed28ebd718510d22186f8a
