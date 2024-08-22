34059f9c1fce7f2fb3d6d9bf664931ed4ab9b1b3
yqh <haihoing@qq.com>
2024-08-22 16:01:20 +0800

    用 clip 重新给图片分类

--- kvision/imgclassify/imgclassify_clip.py
+++ kvision/imgclassify/imgclassify_clip.py
@@ -1,14 +1,44 @@
 #encoding=utf8
+import re, os, sys
+for reldirx, _lidir in (
+        (os.path.dirname(os.path.abspath(__file__)), []), # 这个先，规避加载漏洞。
+        (os.path.dirname(os.path.abspath(".")), [])):
+    while not _lidir and len(reldirx) > 3: # 3 应该所有平台都问题不大。
+        reldirx = os.path.dirname(reldirx) # 只尝试 funclib.py，funclib.pyc 存在版本。
+        _checkfunc = lambda idir: os.path.exists(os.path.join(reldirx, idir, "pythonx", "funclib.py"))
+        _lidir = [os.path.join(reldirx, idir) for idir in os.listdir(reldirx) if _checkfunc(idir)]
+        assert len(_lidir) in (0, 1), _lidir
+        if _lidir: reldirx = os.path.abspath(_lidir[0])
+        del _checkfunc
+    if _lidir: break
+if not reldirx in sys.path: sys.path.append(reldirx) # 放到最后，避免 sys.path.insert
+del _lidir # reldirx 可以继续使用
+if not os.getcwd() in sys.path: # fix linux 软连接的 bug
+    sys.path.append(os.getcwd())
+import time
+from pythonx.funclib import *
+from pythonx.pelib import *
+from PIL import Image, ImageDraw, ImageFont
+
 import os
 import clip
 import torch
 #from torchvision.datasets import CIFAR100
 import cv2
 from PIL import Image
+import numpy as np
 
 # Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, preprocess = clip.load('ViT-B/32', device)
+
+import cn_clip.clip as cn_clip
+from cn_clip.clip import load_from_name, available_models
+print("Available models:", available_models())  
+# Available models: ['ViT-B-16', 'ViT-L-14', 'ViT-L-14-336', 'ViT-H-14', 'RN50']
+
+model_cn, preprocess_cn = load_from_name("ViT-B-16", device=device, download_root='./')
+model_cn.eval()
 
 def test():
     from torchvision.datasets import CIFAR100
@@ -34,7 +64,7 @@ def test():
     values, indices = similarity[0].topk(5)
 
     # Print the result
-    print("\nTop predictions:\n")
+    #print("\nTop predictions:\n")
     for value, index in zip(values, indices):
         print(f"{cifar100.classes[index]:>16s}: {100 * value.item():.2f}%")
 
@@ -47,6 +77,8 @@ def cateclip(image, classes):
     
     classe_ids = [c.split(":")[-1].strip() for c in classes]
     classes = [c.split(":")[0].strip() for c in classes]
+    #print(classe_ids)
+    #print(classes)
     text_inputs = torch.cat([clip.tokenize(f"a photo of a {c}") for c in classes]).to(device)
 
     # Calculate features
@@ -61,12 +93,41 @@ def cateclip(image, classes):
     values, indices = similarity[0].topk(5)
 
     # Print the result
-    print("\nTop predictions:\n")
+    #print("\nTop predictions:\n")
     for value, index in zip(values, indices):
         print(f"{classes[index]:>16s}: {100 * value.item():.2f}%")
-        return classe_ids[index]
+        return classe_ids[index], value.item()
 
-def main():
+def cateclip_cn(image, classes):
+
+    classe_ids = [c.split(":")[-1].strip() for c in classes]
+    classes = [c.split(":")[0].strip() for c in classes]
+
+    #print(classe_ids)
+    #print(classes)
+    image = preprocess(image).unsqueeze(0).to(device)
+    text = cn_clip.tokenize([f"一张 {c} 照片" for c in classes]).to(device)
+
+    with torch.no_grad():
+        image_features = model_cn.encode_image(image)
+        text_features = model_cn.encode_text(text)
+        # 对特征进行归一化，请使用归一化后的图文特征用于下游任务
+        image_features /= image_features.norm(dim=-1, keepdim=True) 
+        text_features /= text_features.norm(dim=-1, keepdim=True)    
+
+        logits_per_image, logits_per_text = model_cn.get_similarity(image, text)
+        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+    # 创建一个 numpy 数组
+    float_array = np.array(probs[0])
+    # 找到最大值
+    max_value = np.max(float_array)
+    # 找到最大值的索引
+    max_index = np.argmax(float_array)
+    #print("Label probs:", probs)  # [[1.268734e-03 5.436878e-02 6.795761e-04 9.436829e-01]]
+    return classe_ids[max_index], max_value
+
+def main(dataset):
     classes = r"""
 animal
 anime
@@ -89,7 +150,7 @@ vehicle
 transportation:vehicle
     """.strip().split("\n")
     classes = [i.strip() for i in classes]
-    cnclasses = r"""
+    classes_cn = r"""
 动物:animal
 动漫:anime
 卡通:anime
@@ -105,9 +166,9 @@ transportation:vehicle
 扫描件:text
 交通工具:vehicle
     """.strip().split("\n")
-    cnclasses = [i.strip() for i in cnclasses]
-    for dir in os.listdir("dataset"):
-        subdir = os.path.join("dataset", dir)
+    classes_cn = [i.strip() for i in classes_cn]
+    for dir in os.listdir(dataset):
+        subdir = os.path.join(dataset, dir)
         if not os.path.isdir(subdir):
             continue
         for idir in os.listdir(subdir):
@@ -118,10 +179,30 @@ transportation:vehicle
             print("***" * 30)
             print(ifile)
             image = Image.open(ifile)
-            print(cateclip(image, classes))
-            break
+            idx1, idv1 = cateclip(image, classes)
+            idx2, idv2 = cateclip_cn(image, classes_cn)
+            #break
+            if idx1 != idx2:
+                continue
+            if idv1 < 0.5 or idv2 < 0.5:
+                continue
 
+            fmd5 = getFileMd5(ifile)[:5]
+            rad = int(fmd5, 16) % 100
+            
+            if rad < 20:
+                targetfile = os.path.join("mydata", "val", idx1, fmd5+".jpg")
+            else:
+                targetfile = os.path.join("mydata", "train", idx1, fmd5+".jpg")
+            fdir = os.path.dirname(targetfile)
+            if not os.path.exists(fdir):
+                os.makedirs(fdir)
+            image.save(targetfile)
+            osremove(ifile)
+
+# 还需要移除相似图片。
 if __name__ == "__main__":
     #test()
-    main()
+    main("dataset")
+    main("valset")
     print("ok")
