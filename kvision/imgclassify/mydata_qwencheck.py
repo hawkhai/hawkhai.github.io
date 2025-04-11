e3f643614f65230bc006192439dcd31e07b38619
yqh <haihoing@qq.com>
2025-04-11 18:18:09 +0800

    check_ksource_dir = lambda x: x and check_pythonx_dir(os.path.join(x, "pythonx"))

--- kvision/imgclassify/mydata_qwencheck.py
+++ kvision/imgclassify/mydata_qwencheck.py
@@ -1,18 +1,20 @@
 #encoding=utf8
 import re, os, sys
-for reldirx, _lidir in (
-        (os.path.dirname(os.path.abspath(__file__)), []), # 这个先，规避加载漏洞。
-        (os.path.dirname(os.path.abspath(".")), [])):
-    while not _lidir and len(reldirx) > 3: # 3 应该所有平台都问题不大。
-        reldirx = os.path.dirname(reldirx) # 只尝试 funclib.py，funclib.pyc 存在版本。
-        _checkfunc = lambda idir: os.path.exists(os.path.join(reldirx, idir, "pythonx", "funclib.py"))
-        _lidir = [os.path.join(reldirx, idir) for idir in os.listdir(reldirx) if _checkfunc(idir)]
-        assert len(_lidir) in (0, 1), _lidir
-        if _lidir: reldirx = os.path.abspath(_lidir[0])
-        del _checkfunc
-    if _lidir: break
-if not reldirx in sys.path: sys.path.append(reldirx) # 放到最后，避免 sys.path.insert
-del _lidir # reldirx 可以继续使用
+from pathlib import Path
+check_pythonx_dir = lambda x: x and os.path.exists(os.path.join(x, "funclib.py"))
+check_ksource_dir = lambda x: x and check_pythonx_dir(os.path.join(x, "pythonx"))
+for reldirx in (os.path.dirname(os.path.abspath(__file__)), # 这个先，规避加载漏洞。
+                os.path.abspath(".")):
+    while len(reldirx) > 3: # 3 应该所有平台都问题不大。
+        if check_ksource_dir(reldirx): break # YES!!
+        reldirx = os.path.dirname(reldirx) # 只尝试 funclib.py，funclib.pyc 存在版本问题。
+    if check_ksource_dir(reldirx): break # YES!!
+for devdirx in [r"E:\kSource", r"D:\kSource", r"C:\kSource"]:
+    if not check_ksource_dir(reldirx):
+        reldirx = devdirx
+if check_ksource_dir(reldirx) and not reldirx in sys.path:
+    sys.path.append(reldirx) # 放到最后，避免 sys.path.insert
+del devdirx # reldirx 可以继续使用
 from pythonx.funclib import *
 from pythonx.ksample import *
 
