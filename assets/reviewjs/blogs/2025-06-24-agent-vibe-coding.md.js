1967a6e4ac76d222b607ba0b0720b7e2709f570f
haidell <haihoing@qq.com>
2025-09-16 01:33:39 +0800

    纯粹的 Vibe Coding 效果非常不可控，最终效果的好坏完全取决于个人驾驭 AI 的能力，在一些生产级项目上滥用很可能留下巨大的技术债务，将成本从开发阶段转移到了调试、测试和部署等后续环节。 **在当前阶段未被完全验证之前，绝不进入下一阶段。**

--- assets/reviewjs/blogs/2025-06-24-agent-vibe-coding.md.js
+++ assets/reviewjs/blogs/2025-06-24-agent-vibe-coding.md.js
@@ -1,1 +1,1 @@
-document.write("2025-08-25: review"); // md5src=fb37a9160fbf43933d16ba3694c6a645
+document.write("2025-08-25: review"); // md5src=ada55c371fcbe6c96f7c1b8e328ac64b
--- assets/reviewjs/blogs/2025-06-25-vibe-coding-best-practices-for-windsurf.md.js
+++ assets/reviewjs/blogs/2025-06-25-vibe-coding-best-practices-for-windsurf.md.js
@@ -1,1 +1,1 @@
-document.write("2025-09-14: review"); // md5src=5288295e2124f1331ced17f3fbb9647e
+document.write("2025-09-15: review"); // md5src=389c9fbe8db053e093529ccb1c83fcdd
