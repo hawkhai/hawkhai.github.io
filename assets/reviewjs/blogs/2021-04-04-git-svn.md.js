3871b327d0aea71e8f2ee65a5173e17908c359fc
yqh <haihoing@qq.com>
2026-02-04 12:01:48 +0800

    git submodule update --init --recursive

--- assets/reviewjs/blogs/2021-04-04-git-svn.md.js
+++ assets/reviewjs/blogs/2021-04-04-git-svn.md.js
@@ -1,1 +1,1 @@
-document.write("2025-10-10: review"); // md5src=a4dc2aaa18794d51910104fc73f15ad6
+document.write("2025-10-10: review"); // md5src=4f2e886be0b5d6d183111f3792bba2e6
--- assets/reviewjs/blogs/2022-09-10-mathgo.md.js
+++ assets/reviewjs/blogs/2022-09-10-mathgo.md.js
@@ -1,1 +1,1 @@
-document.write("2025-09-05: review"); // md5src=66d4c2dcecb6be9a7c7fed770f1946fa
+document.write("2025-09-05: review"); // md5src=dfcef2d535b8a958da6010fcfb9b2f62
