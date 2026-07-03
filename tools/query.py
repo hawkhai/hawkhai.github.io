2067bb319ee92ddb9429ef7a98a99bf40d5ee424
yqh <haihoing@qq.com>
2026-07-03 18:19:58 +0800

    query.py

---
+++ tools/query.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+
+import hashlib
+import json
+import os
+import re
+import sys
+from urllib.parse import parse_qs
+
+
+def add_one_to_integer_string(value):
+    sign = ''
+    if value and value[0] in ('+', '-'):
+        sign = value[0]
+        value = value[1:]
+
+    value = value.lstrip('0')
+    if value == '':
+        value = '0'
+
+    if sign == '-':
+        if value == '0' or value == '1':
+            return '0'
+
+        digits = list(value)
+        borrow = 1
+        for i in range(len(digits) - 1, -1, -1):
+            if borrow <= 0:
+                break
+
+            digit = ord(digits[i]) - 48 - borrow
+            if digit < 0:
+                digit += 10
+                borrow = 1
+            else:
+                borrow = 0
+            digits[i] = chr(digit + 48)
+
+        value = ''.join(digits).lstrip('0')
+        return '0' if value == '' else '-' + value
+
+    digits = list(value)
+    carry = 1
+    for i in range(len(digits) - 1, -1, -1):
+        if carry <= 0:
+            break
+
+        digit = ord(digits[i]) - 48 + carry
+        if digit >= 10:
+            digit -= 10
+            carry = 1
+        else:
+            carry = 0
+        digits[i] = chr(digit + 48)
+
+    value = ''.join(digits)
+    if carry > 0:
+        value = '1' + value
+
+    return value
+
+
+def get_params():
+    if 'QUERY_STRING' in os.environ:
+        return parse_qs(os.environ.get('QUERY_STRING', ''), keep_blank_values=True)
+
+    if len(sys.argv) > 1:
+        return parse_qs(sys.argv[1], keep_blank_values=True)
+
+    return {}
+
+
+def get_input_value(params):
+    for key in ('s', 'str', 'input', 'value'):
+        if key in params:
+            return params[key][0]
+    return None
+
+
+def respond(body, status=None, content_type='text/plain; charset=utf-8'):
+    if status is not None:
+        print('Status: {}'.format(status))
+    print('Content-Type: {}'.format(content_type))
+    print()
+    print(body)
+
+
+def main():
+    params = get_params()
+    input_value = get_input_value(params)
+
+    if input_value is None:
+        respond('Missing input. Use URL: query.py?s=123', '400 Bad Request')
+        return 1
+
+    if re.fullmatch(r'[+-]?\d+', input_value) is None:
+        respond('Input must be an integer string.', '400 Bad Request')
+        return 1
+
+    plus_one = add_one_to_integer_string(input_value)
+    md5_value = hashlib.md5(plus_one.encode('utf-8')).hexdigest()
+
+    if 'json' in params:
+        respond(
+            json.dumps({
+                'input': input_value,
+                'plus_one': plus_one,
+                'md5': md5_value,
+            }, separators=(',', ':')),
+            content_type='application/json; charset=utf-8',
+        )
+        return 0
+
+    respond(md5_value)
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
